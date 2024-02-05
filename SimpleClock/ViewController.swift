316fa75533fbee0e9cfa22896d69090146af5e94
Micah Moore <micah.moore12@gmail.com>
2024-02-04 20:30:35 -0600

    Initial Commit

---
+++ SimpleClock/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SimpleClock
+//
+//  Created by Micah Moore on 2/4/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
